ec0cf40bba28bceb5cec51acbafea67538449920
danil.vladyka1 <danil.vladyka1@gmail.com>
2019-03-08 13:49:06 +0200

    create view console

--- ViewConcole.js
+++ ViewConcole.js
@@ -1,7 +1,9 @@
 class viewConsole{
-    printMap(players,cards,a,b){
+    printMap(players,cards,a,b,c){
         console.log('Player 0: Money: '+players[0].getMoney()+' Position: '+players[0].getPosition());
         console.log(`Player 1: Money: ${players[1].getMoney()} Position: ${players[1].getPosition()}`);
+        console.log(`${a} + ${b}`);
+        console.log(`Go player № : ${c}`);
     }
 };
 
